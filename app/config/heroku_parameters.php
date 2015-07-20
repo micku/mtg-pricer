b76b271abe04c8a731d480a3707790535d015c85
Michele Gargiulo <michele.gargiulo@isobar.com>
2015-07-20 17:25:18 +0200

    Removed Doctrine stuff

--- app/config/heroku_parameters.php
+++ app/config/heroku_parameters.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 $db = parse_url($_ENV['CLEARDB_DATABASE_URL']);
 $container->setParameter('database_driver', 'pdo_mysql');
 $container->setParameter('database_host', $db['host']);
@@ -7,5 +8,6 @@ $container->setParameter('database_port', $db['port']);
 $container->setParameter('database_name', trim($db['path'], '/'));
 $container->setParameter('database_user', $db['user']);
 $container->setParameter('database_password', $db['pass']);
+ */
 
 $container->setParameter('mongodb_server', $_ENV['MONGOLAB_URI']);
